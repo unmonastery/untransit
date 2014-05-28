70ab9ab73f0bbac648ea375b98d0a3e91e982153
mstn <mstn@git.com>
2014-05-28 13:21:29 +0200

    added backbone dependency

--- js/application.js
+++ js/application.js
@@ -6,7 +6,7 @@ define(['chaplin'], function(Chaplin) {
   var Application = Chaplin.Application.extend({
     // Set your application name here so the document title is set to
     // “Controller title – Site title” (see Layout#adjustTitle)
-    title: 'Chaplin Example Application',
+    title: 'unTransit',
     start: function() {
       var args = [].slice.call(arguments);
       // You can fetch some data here and start app
